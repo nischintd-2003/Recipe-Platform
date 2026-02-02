dfe317632a380c304c6cd9ca6eaabf853a8abacb
nischintd-2003 <nischintd@mindfiresolutions.com>
2026-02-02 15:01:09 +0530

    feat(controller): add signup to auth controller

--- apps/backend/src/controllers/auth.controller.ts
+++ apps/backend/src/controllers/auth.controller.ts
@@ -1,4 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+import { AuthService } from "../service/auth.service.js";
+
 export class AuthController {
-  static async signup() {}
-  static async login() {}
+  static async signup(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { username, email, password } = req.body;
+      const user = await AuthService.signup(username, email, password);
+      res.status(201).json({ id: user.id, email: user.email });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
