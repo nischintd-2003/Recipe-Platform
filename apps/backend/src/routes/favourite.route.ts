c737aaecbd6d4aa356ae34ba6e2faa9320e42fb2
nischintd-2003 <nischintd@mindfiresolutions.com>
2026-02-05 11:46:50 +0530

    feat: add swagger docs to favourite route

--- apps/backend/src/routes/favourite.route.ts
+++ apps/backend/src/routes/favourite.route.ts
@@ -4,8 +4,68 @@ import { FavouriteController } from "../controllers/favourite.controller.js";
 
 const favouriteRouter: Router = Router({ mergeParams: true });
 
+/**
+ * @swagger
+ * /recipe/{recipeId}/favourite:
+ *   post:
+ *     summary: Add recipe to favourites
+ *     tags: [Favourite]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: recipeId
+ *         required: true
+ *         schema:
+ *           type: number
+ *     responses:
+ *       201:
+ *         description: Recipe added to favourites
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Recipe not found
+ */
 favouriteRouter.post("/", authMiddleware, FavouriteController.addFavourite);
+
+/**
+ * @swagger
+ * /favourites:
+ *   get:
+ *     summary: Get all favourite recipes of logged-in user
+ *     tags: [Favourite]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of favourite recipes
+ *       401:
+ *         description: Unauthorized
+ */
 favouriteRouter.get("/", authMiddleware, FavouriteController.getUserFavourites);
+
+/**
+ * @swagger
+ * /recipe/{recipeId}/favourite:
+ *   delete:
+ *     summary: Remove recipe from favourites
+ *     tags: [Favourite]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: recipeId
+ *         required: true
+ *         schema:
+ *           type: number
+ *     responses:
+ *       204:
+ *         description: Recipe removed from favourites
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Recipe not found
+ */
 favouriteRouter.delete(
   "/",
   authMiddleware,
