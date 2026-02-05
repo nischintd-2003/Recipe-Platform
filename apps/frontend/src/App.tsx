4d437556de9cae0f1bd0d7467bf77f6289f295b9
nischintd-2003 <nischintd@mindfiresolutions.com>
2026-02-05 17:59:47 +0530

    feat: add basic navbar UI

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Navbar isLoggedIn={true} />
+    </div>
+  );
 }
 
 export default App;
---
+++ apps/frontend/src/components/Navbar.tsx
@@ -0,0 +1,119 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { useState } from "react";
+import { Menu, MenuItem } from "@mui/material";
+
+const navLinks = [
+  { name: "Recipes", path: "/recipes" },
+  { name: "My Recipes", path: "/my-recipes" },
+  { name: "Favourite Recipes", path: "/favorites" },
+];
+
+type NavbarProps = {
+  isLoggedIn: boolean;
+};
+
+const Navbar = ({ isLoggedIn }: NavbarProps) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          {/* Left Section: Logo and Mobile Menu Icon */}
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            {isLoggedIn && (
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2, display: { xs: "flex", md: "none" } }}
+                onClick={handleMenuOpen}
+              >
+                <MenuIcon />
+              </IconButton>
+            )}
+
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ fontWeight: 700, letterSpacing: 1 }}
+            >
+              Fudo
+            </Typography>
+          </Box>
+
+          {/* Middle Section: Navigation Links*/}
+          {isLoggedIn && (
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", md: "flex" },
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 4,
+              }}
+            >
+              {navLinks.map((link) => (
+                <Button
+                  key={link.name}
+                  color="inherit"
+                  sx={{ fontWeight: 500 }}
+                >
+                  {link.name}
+                </Button>
+              ))}
+            </Box>
+          )}
+
+          {/* Right Section: User Avatar or Login Button */}
+          <Box sx={{ ml: "auto" }}>
+            {isLoggedIn ? (
+              <IconButton
+                color="inherit"
+                size="large"
+                aria-label="user-profile"
+              >
+                <AccountCircleIcon />
+              </IconButton>
+            ) : (
+              <Button color="inherit">Login</Button>
+            )}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {/* Mobile Menu */}
+      <Menu
+        anchorEl={anchorEl}
+        open={isMenuOpen}
+        onClose={handleMenuClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        transformOrigin={{ vertical: "top", horizontal: "left" }}
+      >
+        {navLinks.map((link) => (
+          <MenuItem key={link.name} onClick={handleMenuClose}>
+            {link.name}
+          </MenuItem>
+        ))}
+      </Menu>
+    </Box>
+  );
+};
+
+export default Navbar;
