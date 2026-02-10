94f20859234873eb2fe1d638d10c54bc28d03199
nischintd-2003 <nischintd@mindfiresolutions.com>
2026-02-10 14:18:17 +0530

    feat: add utility for cloudinary uploads

---
+++ apps/frontend/src/utils/cloudinary.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+const UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET;
+
+export const uploadImageToCloudinary = async (file: File): Promise<string> => {
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", UPLOAD_PRESET);
+
+  try {
+    const response = await axios.post(
+      `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
+      formData,
+    );
+    return response.data.secure_url;
+  } catch (error) {
+    throw new Error(`Image upload failed : ${error}`);
+  }
+};
